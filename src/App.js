073505f0838d441305f7e57c3fb545ea94ed591f
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-08-11 20:12:38 -0400

    created searchContext for search bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
 import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext';
 
 function App() {
-  let [message, setMessage] = useState('Seach for music')
-  let [search, setSearch] = useState('')
-  let [data, setData] = useState([])
-  const API_URL = 'https://itunes.apple.com/search?term='
+  let [message, setMessage] = useState("Seach for music");
+  let [data, setData] = useState([]);
+  let searchInput = useRef("");
 
-  useEffect(()=>{
-    if(search){
-    const fetchData = async () => {
-      document.title = `${search} music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if(resData.results.length > 0){
-        setData(resData.results)
-      }else{
-        setMessage('Not Found')
-      }
-    }
-    fetchData()
-    }
-  }, [search])
-
+  const API_URL = "https://itunes.apple.com/search?term=";
   const handleSearch = (e, term) => {
     e.preventDefault();
-    setSearch(term)
-  }
+    const fetchData = async () => {
+      document.title = `${term} music`;
+      const response = await fetch(API_URL + term);
+      const resData = await response.json();
+      if (resData.results.length > 0) {
+        setData(resData.results);
+      } else {
+        setMessage("Not Found");
+      }
+    };
+    fetchData();
+  };
 
   return (
     <div className="App">
-      <SearchBar handleSearch={handleSearch}/>
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
+      <SearchBar/>
+      </SearchContext.Provider>
       {message}
       <DataContext.Provider value={data}>
       <Gallery/>
